614ae238acf83caf988809792c09434b20258248
chrisdsmith188 <32425186+chrisdsmith188@users.noreply.github.com>
2022-02-03 17:27:33 -0600

    Add files via upload

---
+++ Models/Producer.cs
@@ -0,0 +1,36 @@
+﻿using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Producer:IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
+
+        public int MyProperty { get; set; }
+        public string ProfilePictureURL { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
+
+
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+
+        [Required(ErrorMessage = "Biography is required")]
+
+
+        public string Bio { get; set; }
+
+        public List<Movie> Movies { get; set; }
+    }
+}
