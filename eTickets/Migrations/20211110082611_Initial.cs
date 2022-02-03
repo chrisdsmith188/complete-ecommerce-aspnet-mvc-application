d1de4025fd1676440186150d588192f2a7c298b6
chrisdsmith188 <32425186+chrisdsmith188@users.noreply.github.com>
2022-02-03 17:36:26 -0600

    Final App

---
+++ eTickets/Migrations/20211110082611_Initial.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MyProperty = table.Column<int>(type: "int", nullable: false),
+                    ProfilePictureURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cinemas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinemas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MyProperty = table.Column<int>(type: "int", nullable: false),
+                    ProfilePictureURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MovieCategory = table.Column<int>(type: "int", nullable: false),
+                    CinemaId = table.Column<int>(type: "int", nullable: false),
+                    ProducerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movie_Cinemas_CinemaId",
+                        column: x => x.CinemaId,
+                        principalTable: "Cinemas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Movie_Producers_ProducerId",
+                        column: x => x.ProducerId,
+                        principalTable: "Producers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Actors_Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    ActorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors_Movies", x => new { x.ActorId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_Actors_Movies_Actor_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Actors_Movies_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actors_Movies_MovieId",
+                table: "Actors_Movies",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_CinemaId",
+                table: "Movie",
+                column: "CinemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_ProducerId",
+                table: "Movie",
+                column: "ProducerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actors_Movies");
+
+            migrationBuilder.DropTable(
+                name: "Actor");
+
+            migrationBuilder.DropTable(
+                name: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "Cinemas");
+
+            migrationBuilder.DropTable(
+                name: "Producers");
+        }
+    }
+}
---
+++ eTickets/Migrations/20211111195644_Fixit.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class Fixit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Actor_ActorId",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Movie_MovieId",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Cinemas_CinemaId",
+                table: "Movie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Producers_ProducerId",
+                table: "Movie");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actor",
+                table: "Actor");
+
+            migrationBuilder.RenameTable(
+                name: "Movie",
+                newName: "Movies");
+
+            migrationBuilder.RenameTable(
+                name: "Actor",
+                newName: "Actors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Movie_ProducerId",
+                table: "Movies",
+                newName: "IX_Movies_ProducerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Movie_CinemaId",
+                table: "Movies",
+                newName: "IX_Movies_CinemaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actors",
+                table: "Actors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Actors_ActorId",
+                table: "Actors_Movies",
+                column: "ActorId",
+                principalTable: "Actors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Movies_MovieId",
+                table: "Actors_Movies",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Cinemas_CinemaId",
+                table: "Movies",
+                column: "CinemaId",
+                principalTable: "Cinemas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Producers_ProducerId",
+                table: "Movies",
+                column: "ProducerId",
+                principalTable: "Producers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Actors_ActorId",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Movies_MovieId",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Cinemas_CinemaId",
+                table: "Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Producers_ProducerId",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actors",
+                table: "Actors");
+
+            migrationBuilder.RenameTable(
+                name: "Movies",
+                newName: "Movie");
+
+            migrationBuilder.RenameTable(
+                name: "Actors",
+                newName: "Actor");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Movies_ProducerId",
+                table: "Movie",
+                newName: "IX_Movie_ProducerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Movies_CinemaId",
+                table: "Movie",
+                newName: "IX_Movie_CinemaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actor",
+                table: "Actor",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Actor_ActorId",
+                table: "Actors_Movies",
+                column: "ActorId",
+                principalTable: "Actor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Movie_MovieId",
+                table: "Actors_Movies",
+                column: "MovieId",
+                principalTable: "Movie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Cinemas_CinemaId",
+                table: "Movie",
+                column: "CinemaId",
+                principalTable: "Cinemas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Producers_ProducerId",
+                table: "Movie",
+                column: "ProducerId",
+                principalTable: "Producers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
